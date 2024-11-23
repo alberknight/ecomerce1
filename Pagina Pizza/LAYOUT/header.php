816adf00fdfe2f1a547536be277ac9532b978f51
alberknight <bernalenin@gmail.com>
2024-11-22 22:55:13 -0600

    se ha eliminado todo los botones inncesarios, y ahora ya esta el primer boton para ver mas en la imagen de una picsa

--- Pagina Pizza/LAYOUT/header.php
+++ Pagina Pizza/LAYOUT/header.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Papu-Zzeria</title>
     <!-- Custom CSS -->
-     <link rel="stylesheet" href="../styles.css">
-     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="../styles.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
 </head>
+
 <body>
     <!-- header region -->
     <header>
         <a href="#" class="logo"><img src="../IMG/LogoPizza.png" alt="logo"></a>
         <ul class="navbar">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#home">About</a></li>
-            <li><a href="#home">Menu</a></li>
-            <li><a href="#home">Contact</a></li>
+            <li><a href="#home">Inicio</a></li>
+            <li><a href="#menu">Menu</a></li>
         </ul>
 
         <div class="h-icons">
-            <a href="#"><i class='bx bx-search'></i></a>
             <a href="#"><i class='bx bx-cart-alt'></i></a>
             <div class="bx bx-menu" id="menu-icon"></div>
         </div>
@@ -29,76 +28,40 @@
     <section class="home" id="home">
         <div class="home-text">
             <h1><span>Bienvenido</span> Al mundo de las PINCHIS Pizzas del TEC!!!</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing. <br>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam dolores error quisquam. Corrupti, libero quos?</p>
-            <a href="#" class="btn">Choose a Pizza</a>
+            <p>Te invitamos a disfrutar todas las combinaciones de sabores que puedes encontrar.
+                En nuestro menú encontrarás las últimas tendencias de cocina internacional, no pierdas la oportunidad de probar todas nuestras pizzas con las diferentes variedades de salsas y toppings, estamos seguros que experimentaras un boom en tu paladar.</p>
+            <a href="#" class="btn">Iniciar Sesion</a>
         </div>
         <div class="home-img">
             <img src="../IMG/Background.png" alt="home">
         </div>
     </section>
 
-    <section class="container">
-        <div class="main-text">
-            <h2>Ingredientes</h2>
-            <p>Solo lo mejor para TI</p>
-        </div>
-        <div class="container-box">
-            <div class="c-mainbox">
-                <div class="container-img">
-                    <img src="../img/masa.png" alt="box1">
-                </div>
-                <div class="container-text">
-                    <p>Delicate Dought</p>
-                </div>
-            </div>
-            <div class="c-mainbox">
-                <div class="container-img">
-                    <img src="../img/toma.png" alt="box1">
-                </div>
-                <div class="container-text">
-                    <p>Fresh Tomatoes</p>
-                </div>
-            </div>
-            <div class="c-mainbox">
-                <div class="container-img">
-                    <img src="../img/cheese.png" alt="box1">
-                </div>
-                <div class="container-text">
-                    <p>Alpine Cheese</p>
-                </div>
-            </div>
-            <div class="c-mainbox">
-                <div class="container-img">
-                    <img src="../img/sal.png" alt="box1">
-                </div>
-                <div class="container-text">
-                    <p>Italian Sauce</p>
-                </div>
-            </div>
-        </div>
-    </section>
 
     <!-- about section -->
-     <section class="about" id="about">
+    <section class="about" id="about">
         <div class="about-img">
             <img src="../img/pizza.png" alt="about-img">
         </div>
         <div class="about-text">
-            <h2>The Delicious Food <br> For a Good Mood</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, saepe. <br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, est! Blanditiis facilis, libero labore numquam eaque maxime ipsam harum possimus, repellendus quam iure hic distinctio, temporibus perferendis necessitatibus incidunt inventore!</p>
-            <a href="#" class="btn">Choose a Pizza</a>
+            <h2>La deliciosa comida Para un buen <br>estado de ánimo</h2>
+            <p>Disfruta cada momento con nuestras pizzas, creadas con ingredientes frescos y una mezcla de sabores irresistibles.
+                Cada bocado está diseñado para llenarte de energía, felicidad y satisfacción. Porque sabemos que una buena pizza no solo alimenta,
+                también transforma tu día. ¡Deja que el aroma, el sabor y la calidad de nuestras recetas eleven tu ánimo a otro nivel!"</p>
         </div>
-     </section>
+    </section>
 
-     <!-- menu section -->
+    <!-- menu section -->
     <section class="menu" id="menu">
         <div class="main-text">
-            <h2>Most Popuiar Pizza</h2>
-            <p>We have selected for You <br> the most exquisite tastes around the world</p>
+            <h1>Menu Popuiar de Pizzas Deliciosas</h1>
         </div>
         <div class="menu-content">
             <div class="row">
-                <img src="../img/margarita.jpg" alt="main-product1">
+                <div class="menu-image">
+                    <img src="../img/margarita.jpg" alt="main-product1">
+                    <a href="../pizza_margarita.php" class="btn">Ver más</a>
+                </div>
                 <div class="menu-text">
                     <div class="menu-left">
                         <h4>Margarita</h4>
@@ -116,6 +79,7 @@
                     <a href="#"><i class='bx bxs-star'></i></a>
                 </div>
             </div>
+
             <div class="row">
                 <img src="../img/mexicana.png" alt="main-product2">
                 <div class="menu-text">
@@ -193,6 +157,25 @@
                 </div>
             </div>
             <div class="row">
+                <img src="../img/hawa.jpg" alt="main-product4">
+                <div class="menu-text">
+                    <div class="menu-left">
+                        <h4>Hawaiana</h4>
+                    </div>
+                    <div class="menu-right">
+                        <h5>$17.00</h5>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, quos?</p>
+                <div class="star">
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                    <a href="#"><i class='bx bxs-star'></i></a>
+                </div>
+            </div>
+            <div class="row">
                 <img src="../img/chicken.jpg" alt="main-product4">
                 <div class="menu-text">
                     <div class="menu-left">
@@ -212,48 +195,13 @@
                 </div>
             </div>
         </div>
-    </section>
-
-    <!-- contact section -->
-     <section class="contact" id="contact">
-        <div class="main-contact">
-            <div class="contact-content">
-            <h4>Services</h4>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#menu"></a>Menu</li>
-            <li><a href="#contact">Contact</a></li>
-            </div>
-            <div class="contact-content">
-            <h4>Services</h4>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#menu"></a>Menu</li>
-            <li><a href="#contact">Contact</a></li>
-            </div>
-            <div class="contact-content">
-            <h4>Services</h4>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#menu"></a>Menu</li>
-            <li><a href="#contact">Contact</a></li>
-            </div>
-            <div class="contact-content">
-            <h4>Services</h4>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#menu"></a>Menu</li>
-            <li><a href="#contact">Contact</a></li>
-            </div>
-        </div> 
-     </section>
-     <div class="last-text">
-        <p>Copyright 2022. All rights reserved.</p>
-     </div>
-         <!-- Scroll top -->
-    <script src="https://unpkg.com/scrollreveal"></script>
+        <div class="last-text">
+            <p>Copyright 2022. All rights reserved.</p>
+        </div>
+        <!-- Scroll top -->
+        <script src="https://unpkg.com/scrollreveal"></script>
         <!-- Custom JS -->
         <script type="text/javascript" src="../pizza.js"></script>
-    <a href="#home" class="scroll-top">
-    <i class='bx bx-up-arrow-circle'></i>
-    </a>
+        <a href="#menu" class="scroll-top"></a>
+        <i class='bx bx-up-arrow-circle'></i>
+        </a>
